4fe5e8e005f030c0e8d30546b1e9c20c904d39e5
Karimjon0 <abidjonovkarimjon@gmail.com>
2024-05-15 15:16:16 +0500

    Impliment Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,104 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//----------------------------------------
+// Great Code Team (c) All rights reserved
+//----------------------------------------
+
+using ClothesShop.Models;
+using ClothesShop.Service;
+
+IClothesService clothesService = new ClothesService();
+bool isContinue = true;
+do
+{
+    Console.WriteLine("|    Menu     |");
+    Console.WriteLine("|----------------------------------------------------------------------------------------------------------------------------------|");
+    Console.WriteLine("|1. Get clothes            |     2. Get all clothes        |     3. Add clothes          |     4. Add range clothes                |");
+    Console.WriteLine("|----------------------------------------------------------------------------------------------------------------------------------|");
+    Console.WriteLine("|5. Update one clothes     |     6. Delete one clothes     |     7. Purchase product     |     8. Sold information product         |");
+    Console.WriteLine("|----------------------------------------------------------------------------------------------------------------------------------|");
+    Console.Write("Enter command ");
+    string command = Console.ReadLine();
+    if (command.Contains("1") is true)
+    {
+        Console.Write("Enter id: ");
+        int id = Convert.ToInt32(Console.ReadLine());
+        clothesService.ReadClothes(id);
+    }
+    if (command.Contains("2") is true)
+    {
+        clothesService.ReadAllClothes();
+    }
+    if (command.Contains("3") is true)
+    {
+        clothesService.InsertClothes(InputClothesInformation());
+    }
+    if (command.Contains("4") is true)
+    {
+        Console.Write("Enter the number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        List<Clothes> newClothes = new List<Clothes>();
+        for (int itiration = 1; itiration <= number; itiration++)
+        {
+            newClothes.Add(InputClothesInformation());
+        }
+        clothesService.InsertRangeClothes(newClothes);
+    }
+    if (command.Contains("5") is true)
+    {
+        clothesService.Update(1, InputClothesInformation());
+    }
+    if (command.Contains("6") is true)
+    {
+        Console.Write("Enter the id to delete ");
+        int id = Convert.ToInt32(Console.ReadLine());
+        clothesService.Delete(id);
+    }
+    if (command.Contains("7") is true)
+    {
+        Console.Write("Enter product type: ");
+        string type = Console.ReadLine();
+        clothesService.Purchase(type);
+    }
+    if (command.Contains("8") is true)
+    {
+        clothesService.SoldInformation();
+    }
+    Console.Write("Is Continue ");
+    string isCommand = Console.ReadLine();
+    if (isCommand.ToLower().Contains("no") is true)
+    {
+        isContinue = false;
+    }
+    else if (isCommand.ToLower().Contains("yes") is true)
+    {
+        isContinue = true;
+        Console.Clear();
+    }
+    else
+    {
+        isContinue = false;
+        Console.WriteLine("The command was issued incorrectly.");
+    }
+} while (isContinue is true);
+
+static Clothes InputClothesInformation()
+{
+    Clothes clothes = new Clothes();
+    Console.Write("Enter the id: ");
+    clothes.Id = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter the model: ");
+    clothes.Model = Console.ReadLine();
+    Console.Write("Enter the type: ");
+    clothes.Type = Console.ReadLine();
+    Console.Write("Enter the size: ");
+    clothes.Size = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter the cost: ");
+    clothes.Cost = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter the color: ");
+    clothes.Color = Console.ReadLine();
+    Console.Write("Enter the discraption: ");
+    clothes.Discraption = Console.ReadLine();
+    Console.Write("Enter the amount: ");
+    clothes.Amount = Convert.ToInt32(Console.ReadLine());
+    return clothes;
+}
